251413634f50bf8092d8630d47b65bb019715941
Анастасия <understooooood@mail.ru>
2022-10-11 18:36:10 +0300

    решение задачb 13 домашнего задания

--- 13 задача домашнего задания/Program.cs
+++ 13 задача домашнего задания/Program.cs
@@ -3,9 +3,9 @@
 Console.Clear();
 Console.WriteLine("Введите число: ");
 int num = int.Parse(Console.ReadLine()!);
-int a = num % 10;
-if (a = num % 10)
+if (num > 99)
 {
+ int a = num % 10;
  Console.WriteLine($"Вторая цифра числа {num} -> {a}");   
 }
 else
